e46ae8ce1402aa3e8fd0ac6c17d92c13775b5cd4
csuau <csuaubou@gmail.com>
2022-07-15 19:16:31 +0200

    init project / add HTML structur

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+console.log('>> Ready :)');
+const searchBtn = document.querySelector('.js_searchBtn');
+const userSearch = document.querySelector('.js_userSearch');
+const resultList = document.querySelector('.js_resultList');
+const favoritesList = document.querySelector('.js_favoriteList');
+const resetBtn = document.querySelector('.js_resetBtn');
