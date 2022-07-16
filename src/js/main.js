9cb8f40a4088401e9c8125d9f3a0cdb29828450f
csuau <csuaubou@gmail.com>
2022-07-16 16:20:18 +0200

    add render function finished

--- src/js/main.js
+++ src/js/main.js
@@ -20,18 +20,28 @@ function getItems (value) {
         .then((response) => response.json())
         .then(({ data }) => {
 
-            data.forEach(({ title, images }) => {
-                renderResults(title);
-
+            data.forEach(({ title, images: { jpg: { image_url } } }) => {
+                renderResults(title, image_url);
+                console.log(image_url);
             });
 
         });
 }
-function renderResults (title) {
+function renderResults (title, image_url) {
     const liElement = document.createElement('li');
     // liElement.classList.add('');
-    liElement.innherText = title;
 
+
+    const divResult = document.createElement("div");
+    const imgElement = document.createElement("img");
+    imgElement.setAttribute("src", image_url);
+    const h2Element = document.createElement("h2");
+    const titleResult = document.createTextNode(title);
+    h2Element.appendChild(titleResult);
+    liElement.appendChild(h2Element);
+    liElement.appendChild(divResult);
+    divResult.appendChild(imgElement);
+    resultList.appendChild(liElement);
 
 
 }
