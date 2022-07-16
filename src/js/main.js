8fb2476baf69402ddc11e53d1315267661c1964a
csuau <csuaubou@gmail.com>
2022-07-16 13:23:11 +0200

    add fetch and init render function

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,34 @@ const userSearch = document.querySelector('.js_userSearch');
 const resultList = document.querySelector('.js_resultList');
 const favoritesList = document.querySelector('.js_favoriteList');
 const resetBtn = document.querySelector('.js_resetBtn');
+
+function manejadora (ev) {
+    ev.preventDefault();
+    const value = userSearch.value;
+    if (value) {
+        getItems(value);
+    }
+}
+function getItems (value) {
+    console.log('tester');
+    fetch(`https://api.jikan.moe/v4/anime?q=${value}`)
+        .then((response) => response.json())
+        .then(({ data }) => {
+
+            data.forEach(({ title, images }) => {
+                renderResults(title);
+
+            });
+
+        });
+}
+function renderResults (title) {
+    const liElement = document.createElement('li');
+    // liElement.classList.add('');
+    liElement.innherText = title;
+
+
+
+}
+searchBtn.addEventListener('click', manejadora);
+
